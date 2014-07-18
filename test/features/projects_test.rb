176e45955cc514e0d3dd3359c98f3fea8eddcd35
Christian Kyony <ckyony@changamuka.com>
2014-07-18 10:34:21 +0200

    Ensure names are specified when creating projects

--- test/features/projects_test.rb
+++ test/features/projects_test.rb
@@ -1,9 +1,14 @@
 require "test_helper"
 
 feature "Projects" do
-  scenario "creating projects" do
+
+  before  do
     visit root_path
     click_link "New Project"
+  end
+
+
+  scenario "creating projects" do
 
     fill_in "Name", with: 'TextMate 2'
     fill_in "Description", with: "A text-editor for OS X"
@@ -19,4 +24,12 @@ feature "Projects" do
 
 
   end
+
+  scenario  "Can not create a project without a name" do
+    click_button 'Create Project'
+
+    assert_content page, "Project has not been created."
+    assert_content page, "Name can't be blank"
+  end
+
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe Project do
   let(:project) { Project.new }
 
-  it "must be valid" do
-    project.must_be :valid?
+  it "must be invalid" do
+    project.must_be :invalid?
   end
 end

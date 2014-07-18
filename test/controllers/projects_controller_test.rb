de2a46d5e11b9126433b144b7d71efc8255b8660
Christian Kyony <ckyony@changamuka.com>
2014-07-18 14:11:22 +0200

    Display an error for a missing project

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,4 +1,10 @@
 require "test_helper"
 
 describe ProjectsController do
+  it "display an error for a missing project" do
+    get :show, id: "not-here"
+    assert_redirected_to projects_path
+    message = "The project you were looking for could not be found."
+    assert_equal flash[:alert], message
+  end
 end

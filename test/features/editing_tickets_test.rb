d165709cbf0a7cfa723ce2735b19cc383b1460d1
Christian Kyony <ckyony@changamuka.com>
2014-07-18 19:51:10 +0200

    I can now edit tickets

---
+++ test/features/editing_tickets_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+feature "Editing Tickets" do
+  let(:project ) {projects(:textmate) }
+  let(:ticket) { tickets(:feature_request)}
+  
+  before :each do
+    visit root_path
+    click_link project.name
+    click_link ticket.title
+    click_link 'Edit Ticket'
+  end
+
+  scenario "Updating a ticket" do
+    fill_in 'Title', with: 'Make it really shiny!'
+    click_button "Update Ticket"
+    assert_content "Ticket has been updated."
+
+    within('#ticket h2') do |args|
+      assert_content "Make it really shiny!"
+    end
+
+    refute_content ticket.title
+  end
+
+  scenario "I cannot update a ticket with invalid information" do 
+    fill_in "Title", with: ''
+    click_button "Update Ticket"
+
+    assert_content "Ticket has not been updated."
+
+    
+  end
+end
